6788ea24492476e4120b5530af106faa183af8db
JealineA <jeala81@yahoo.com>
2024-11-14 21:14:41 -0500

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,16 @@
+# Input for Area formula
+
+Length = float(input ("Enter length the length of the rectangle: "))
+
+Width = float(input("Enter the width of the rectangle"))
+
+#Calculate the area of the rectangle
+
+Area = Length * Width
+
+#Display the output
+
+print("The are of the rectangle is: ", Area)
+
+
+
